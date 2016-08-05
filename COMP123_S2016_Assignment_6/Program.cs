c8a3438f8e15ee79a77142648039a4134cff64e4
Chenyuan Xie <1147719157xie@gmail.com>
2016-08-05 11:26:01 -0400

    Initial Commit

--- COMP123_S2016_Assignment_6/Program.cs
+++ COMP123_S2016_Assignment_6/Program.cs
@@ -16,7 +16,7 @@ namespace COMP123_S2016_Assignment_6
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
